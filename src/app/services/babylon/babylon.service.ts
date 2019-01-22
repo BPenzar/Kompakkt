88a3e5784ed322a720f173988e87879aef55d467
Bruno <bruno@Brunos-MBP.gigaset.lan>
2019-01-22 18:54:04 +0100

    aua

--- src/app/services/babylon/babylon.service.ts
+++ src/app/services/babylon/babylon.service.ts
@@ -95,8 +95,13 @@ export class BabylonService {
             }
 
             // Annotation-Marker-Size
-            // this.scene.getMeshesByTags('label', mesh => console.log(mesh));
-            // this.scene.getMeshesByTags('plane', mesh => console.log(mesh.scaling));
+            // this.scene.getMeshesByTags('label', mesh => console.log(mesh.scalingDeterminant));
+            this.scene.getMeshesByTags('plane', mesh => console.log(mesh.scaling));
+            console.log(this.getActiveCamera().interaxialDistance);
+            console.log(this.getActiveCamera().onViewMatrixChangedObservable);
+            console.log(this.getActiveCamera().position);
+            console.log(this.getActiveCamera().viewport);
+            console.log(this.getActiveCamera().upVector);
 
           });
 
