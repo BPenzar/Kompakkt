d17f4ce1037554c940345fa7cc32b7cf75780c4c
Bruno <bruno@Brunos-MBP.gigaset.lan>
2019-01-28 11:48:53 +0100

    bla! !

--- src/app/services/camera/camera.service.ts
+++ src/app/services/camera/camera.service.ts
@@ -59,7 +59,7 @@ export class CameraService {
         this.arcRotateCamera.attachControl(newCanvas, false);
 
         // VR BUTTON
-        // this.vrHelper = this.babylonService.createVRHelper();
+        this.vrHelper = this.babylonService.createVRHelper();
       }
     });
   }
@@ -67,7 +67,7 @@ export class CameraService {
   // VR BUTTON
   public createVrHelperInCamera(): void {
     
-    this.vrHelper = this.babylonService.createVRHelper();
+    // this.vrHelper = this.babylonService.createVRHelper();
     this.babylonService.getVRHelper().enterVR();
   }
 
